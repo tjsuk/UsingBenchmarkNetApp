033dda484fbf0265bfdb33c93fe77f426269c096
Trevor Smith <156339246+tjsuk@users.noreply.github.com>
2024-03-27 21:06:57 +0000

    Add project files.

---
+++ UsingBenchmarkNet/Program.cs
@@ -0,0 +1,38 @@
+﻿// Install-Package BenchmarkDotNet
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+namespace UsingBenchmarkNet;
+
+[MemoryDiagnoser]
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<Program>();
+    }
+
+    [Benchmark]
+    public void LoopString()
+    {
+        var str = "Hello World!";
+        for (int i = 0; i < 1000; i++)
+        {
+            str += i;
+        }
+    }
+
+    [Benchmark]
+    public void LoopStringBuilder()
+    {
+        var sb = new StringBuilder();
+        sb.Append("Hello World!");
+        for (int i = 0; i < 1000; i++)
+        {
+            sb.Append(i);
+        }
+    }
+
+}
